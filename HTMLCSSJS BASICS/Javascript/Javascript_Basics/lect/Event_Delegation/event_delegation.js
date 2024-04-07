af6a0da8bcd3ee9e55ad4d1092d9231a54ac4f35
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-08 00:51:44 +0530

    eventdelegation

--- HTMLCSSJS BASICS/Javascript/Javascript_Basics/lect/Event_Delegation/event_delegation.js
+++ HTMLCSSJS BASICS/Javascript/Javascript_Basics/lect/Event_Delegation/event_delegation.js
@@ -3,5 +3,11 @@
 let parent = document.querySelector("#parent");
 parent.addEventListener("click",function(event){
    console.log(event);
+   if(event.target.id == 'play'){
+    console.log("Play");
+   }
+   else if(event.target.id == 'pause'){
+    console.log('Pause');
+   }
 
 });
