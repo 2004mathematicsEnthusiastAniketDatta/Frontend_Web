dcb05fd0093d426f8fde5cfcebd8ce2902ca89db
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-11 14:56:29 +0530

    img

--- HTMLCSSJS BASICS/Javascript/Javascript_Basics/DOM/imgDOM.js
+++ HTMLCSSJS BASICS/Javascript/Javascript_Basics/DOM/imgDOM.js
@@ -1,1 +1,3 @@
-let img=
+let img = document.createElement("img");
+img.src = "https://cdn.pixabay.com/photo/2017/09/08/10/00/tree-7368850_960_720.jpg";
+document.querySelector('#main').appendChild(img);
