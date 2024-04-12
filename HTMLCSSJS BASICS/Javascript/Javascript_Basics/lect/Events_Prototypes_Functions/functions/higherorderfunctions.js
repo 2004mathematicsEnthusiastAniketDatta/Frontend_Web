3604647db3819842ee58af89ef59c8971cdb86ea
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-12 13:37:49 +0530

    higher order functions

--- HTMLCSSJS BASICS/Javascript/Javascript_Basics/lect/Events_Prototypes_Functions/functions/higherorderfunctions.js
+++ HTMLCSSJS BASICS/Javascript/Javascript_Basics/lect/Events_Prototypes_Functions/functions/higherorderfunctions.js
@@ -1,7 +1,10 @@
 //Higher order functions:  A higher order function is a function that takes one or more functions as arguments, or returns a function as its result.
 "use strict";
-function abcd(){
-    return function efgh{
-        
-    }
+
+function salary(num){
+    return num*.7
 }
+setTimeout(salary,300)
+
+const mySalaries=[20,30,40,20,300,400];
+mySalaries.filter(n => n > 50)
