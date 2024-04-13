37fa61fe6144bcbc8dfc3059449afa19c38b8cc5
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-13 21:53:06 +0530

    objectsintsc

---
+++ TypeScript/objects/objects.ts
@@ -0,0 +1,26 @@
+const User ={
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+    isActive: true
+}
+
+// let name : string, age : number, email : string, isActive : boolean;
+// function createUser({name  , age: number, email : string, isActive:boolean}){
+//     console.log(name, age, email, isActive);
+// }
+
+// createUser({ name: "John",
+// age: 30,
+// email: "john@example.com",
+// isActive: true})
+
+function createUser({  age: number, name,email : string, isActive:boolean}){
+    return { name: "John",
+    age: 30,
+    email: "john@example.com",
+    isActive: true};
+}
+console.log(createUser(User));
+
+export {};
