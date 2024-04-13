14c53ca6530f58a8399b3f8aacaed5141de2e9ee
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-13 20:43:02 +0530

    god

--- TypeScript/types/function_types/functions.js
+++ TypeScript/types/function_types/functions.js
@@ -3,6 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function addsum(num) {
     return num + 2;
 }
-
-function getUpper()
 addsum(5);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper('hello');
+// TypeScript also supports type inference, which means that you don't have to explicitly define the type of the parameter or return value. TypeScript will automatically infer the type based on the value you pass to the function.
+function signUpUser(username, email, password, ispaid) {
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Password :', password);
+    console.log('Is Paid:', ispaid);
+}
+;
+signUpUser('john', 'john@gmail.com', '123456', true);
--- TypeScript/types/function_types/functions.ts
+++ TypeScript/types/function_types/functions.ts
@@ -2,5 +2,17 @@ function addsum(num : number){
     return num + 2;
 }
 addsum(5);
+function getUpper(val : string){
+    return val.toUpperCase();
+}
+getUpper('hello');
+// TypeScript also supports type inference, which means that you don't have to explicitly define the type of the parameter or return value. TypeScript will automatically infer the type based on the value you pass to the function.
 
+function signUpUser(username: string, email: string, password:string , ispaid : boolean){
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Password :', password);
+    console.log('Is Paid:', ispaid);
+};
+signUpUser('john', 'john@gmail.com','123456', true);
 export{}
