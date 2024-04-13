1be544209670d555097a926ca55f3f3204069002
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-13 23:13:15 +0530

    codeworshipping

--- TypeScript/07_Arrays/Arrays.js
+++ TypeScript/07_Arrays/Arrays.js
@@ -1,3 +1,7 @@
 var data = [1, 2, 3, '4', '5', '6'];
 console.log(typeof (data[5]), typeof (data[0]));
 console.log(data);
+
+var seatAllotment;
+seatAllotment = "aisle";
+console.log(seatAllotment);
--- TypeScript/07_Arrays/Arrays.ts
+++ TypeScript/07_Arrays/Arrays.ts
@@ -2,3 +2,7 @@
 const data: (string | number)[] = [1, 2, 3, '4', '5', '6'];
 console.log( typeof(data[5]) , typeof(data[0]));
 console.log(data);
+
+let seatAllotment: "aisle" | "middle" | "window";
+seatAllotment = "aisle";
+console.log(seatAllotment);
