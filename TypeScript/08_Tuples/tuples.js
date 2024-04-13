c923f97c15663760185a7365fb790ce32f37cdc6
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-14 02:08:35 +0530

    commit

---
+++ TypeScript/08_Tuples/tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = ['Typescript', 'transpiled', 'Javascript'];
+var tUser;
+tUser = ['type', 1, true];
+var rgb = [255, 123, 112];
+console.log(rgb);
--- TypeScript/08_Tuples/tuples.ts
+++ TypeScript/08_Tuples/tuples.ts
@@ -1,2 +1,24 @@
 const user : (string | number)[] = ['Typescript','transpiled','Javascript'];
+let  tUser : [string , number , boolean];
+tUser = ['type', 1 ,true];
+let rgb :[number,number,number] = [255,123,112];
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export{}
