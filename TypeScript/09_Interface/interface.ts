24e5dbf04e93cb3d739ba9f1b9e47e7f0a72142d
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-14 21:14:02 +0530

    interfaces

--- TypeScript/09_Interface/interface.ts
+++ TypeScript/09_Interface/interface.ts
@@ -1,6 +1,19 @@
 interface User {
+    readonly dbId: number;
     email: string;
     userId: number;
+    // startTrial: () => string; in typescriptor startTrial() =>{
+//     return "Trial started";
+// }
+    starting(): string;
+    getCoupon(couponname: string): number;
 }
 
-const typescriptor:
+const typescriptor: User = {dbId:1234 ,email: "typescriptor@.js" , userId: 2211,starting(){
+    return "Starting";
+},getCoupon(couponname: string){return 10;}};
+
+typescriptor.email = "typescriptor@.ts";
+console.log(typescriptor);
+console.log(typescriptor.getCoupon("typescriptor"));
+console.log(typescriptor.starting());
