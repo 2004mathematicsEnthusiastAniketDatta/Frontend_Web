9db8fb6b38ee759ba23301bea6d0da136bc870dc
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-15 02:44:33 +0530

    getters_setters

--- TypeScript/11_classes/classes.js
+++ TypeScript/11_classes/classes.js
@@ -1,14 +1,12 @@
-class User {
-    email: string;
-    name: string;
-    readonly city: string = 'Kolkata';
-    constructor(email: string, name: string) {
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.city = 'Kolkata';
         this.email = email;
         this.name = name;
     }
-}
-
-const user = new User('<EMAIL>', '<NAME>');
+    return User;
+}());
+var user = new User('<EMAIL>', '<NAME>');
 console.log(user.email); // <EMAIL>
 console.log(user.city); // Kolkata
 // user.city = 'Mumbai'; // Error: Cannot assign to 'city' because it is a read-only property.
