48f9f075c13750992e80894df3cdc9e55a82422b
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-14 22:26:30 +0530

    ok

--- TypeScript/10_interface_extends_and_types/interface_types.ts
+++ TypeScript/10_interface_extends_and_types/interface_types.ts
@@ -1,3 +1,27 @@
-interface User{
-    
+interface User {
+    readonly dbId: number;
+    email: string;
+    userId: number;
+    // startTrial: () => string; in typescriptor startTrial() =>{
+//     return "Trial started";
+// }
+    starting(): string;
+    getCoupon(couponname: string): number;
 }
+
+interface User {
+    githubToken : string;
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner";
+}
+
+const typescriptor: User = {dbId:1234 ,email: "typescriptor@.js" , userId: 2211,starting(){
+    return "Starting";
+},getCoupon(couponname: string){return 10;}};
+
+typescriptor.email = "typescriptor@.ts";
+console.log(typescriptor);
+console.log(typescriptor.getCoupon("typescriptor"));
+console.log(typescriptor.starting());
