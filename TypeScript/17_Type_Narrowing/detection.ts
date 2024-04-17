2c6a032a71c9aecd0e517965b5f1b3a54ccbf7d8
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-17 11:47:43 +0530

    detect

---
+++ TypeScript/17_Type_Narrowing/detection.ts
@@ -0,0 +1,13 @@
+function detecttheTypes(val : number |  string | number [] | string [] | object | null)
+{
+    if (typeof val === 'number')
+         {return 'number';}
+    if (typeof val ==='string') 
+        {return'string';}
+    if (Array.isArray(val)) 
+        {return 'array';}
+    if (typeof val === 'object')
+         {return 'object';}
+    if(typeof val === null)
+        {return 'null';}
+}
