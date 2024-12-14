089806c7be3512f99e56a9b30d0bca7530849fc0
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-12-14 09:31:21 +0000

    react

--- reactjsPrac/src/App.jsx
+++ reactjsPrac/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Intro from './components/Intro'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <main className='flex justify-center items-center'>Hello World</main>
+      <main className='flex justify-center items-center h-screen w-full'><div className='Welcome text-red-500  text-1xl'><h1>HELLO WORLD</h1>
+      <Intro />
+      </div>
+      </main>
     </>
   )
 }
---
+++ reactjsPrac/src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+        <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit explicabo vitae ex, temporibus voluptatum ipsum aut qui sequi cum accusantium.</h3>
+        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo iusto dolore saepe vitae dolorem eum! Ut magnam, molestias quaerat quisquam quod numquam itaque porro explicabo accusantium ab voluptatum quis minima optio commodi fuga ad sunt consequatur, saepe, sapiente nam dolores veniam? Vel, placeat. Excepturi quod eveniet laborum ullam consectetur obcaecati suscipit. Ipsam ratione atque commodi incidunt neque consequatur quo magni nulla odio, eligendi inventore impedit quibusdam libero eos obcaecati officiis. Iure sint eveniet, consequatur ex, voluptate perferendis maxime a porro harum voluptatibus laboriosam velit totam similique, error sapiente quis non facere et veritatis sit! Quibusdam rerum assumenda dicta similique quos eum cumque vel quasi tempora, doloremque aperiam! Labore magnam quaerat voluptatum vel dignissimos odit iure distinctio quidem quos alias laudantium quam ex fugiat, itaque odio expedita sed? Vel aspernatur culpa vitae labore odit quae ipsam doloremque cum, dicta reiciendis commodi temporibus asperiores perspiciatis saepe ad nihil neque. Ea, exercitationem reiciendis!</h4>
+        <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam amet explicabo, esse dolor deleniti provident corporis aspernatur quos eveniet, iusto sunt vitae ducimus repellendus eum minima et fugit, architecto voluptate.</h5>
+        <h6>Lorem.</h6>
+        <div className='flex justify-center'>
+        <img src="../src/assets/react.svg" alt="image" className='h-[500px] w-[500px] '/>
+        </div>
+        <a href="mailto:react@gmail.com">mail</a>
+        
+    </div>
+  )
+}
+
+export default Intro
